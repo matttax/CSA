96a43365122f34c4b039d745ce08e7179e7a2c19
Maksim Romanov <67387151+matttax@users.noreply.github.com>
2021-10-24 21:49:26 +0300

    Update sphere.h

--- CSA_HW2/sphere.h
+++ CSA_HW2/sphere.h
@@ -1,22 +1,31 @@
+// sphere.h - содержит описание сферы и её интерфейса
+
 #include "rand.h"
 #include "shape.h"
 
 using namespace std;
 
-#ifndef CSA_HW2_SPHERE_H
-#define CSA_HW2_SPHERE_H
+#ifndef __sphere__
+#define __sphere__
 
 
 class sphere : public shape {
 private:
-    int r;
+    int r; // Радиус
 public:
     virtual ~sphere() {}
+
+    // Ввод параметров сферы из файла
     virtual void in(ifstream &ifdt);
+
+    // Случайная генерация параметров сферы
     virtual void in_rand();
+
+    // Вывод параметров сферы в поток
     virtual void out(ofstream &ofst);
+
+    // Вычисление площади поверхности сферы
     virtual double sq_surf();
 };
 
-
-#endif //CSA_HW2_SPHERE_H
+#endif //__sphere__
