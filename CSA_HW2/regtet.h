448315124249cf092d10c8ac41c4ee528c414af1
Maksim Romanov <67387151+matttax@users.noreply.github.com>
2021-10-24 21:39:56 +0300

    Update regtet.h

--- CSA_HW2/regtet.h
+++ CSA_HW2/regtet.h
@@ -1,21 +1,31 @@
+// regtet.h - содержит описание правильного тетраэдра и его интерфейса
+
 #include "rand.h"
 #include "shape.h"
 
 using namespace std;
 
-#ifndef CSA_HW2_REGTET_H
-#define CSA_HW2_REGTET_H
+#ifndef __regtet__
+#define __regtet__
 
 
 class regtet : public shape {
 private:
-    int edge;
+    int edge; // Сторона
 public:
     virtual ~regtet() {}
+
+    // Ввод параметров правильного тетраэдра из файла
     virtual void in(ifstream &ifdt);
+
+    // Случайная генерация параметров правильного тетраэдра
     virtual void in_rand();
+
+    // Вывод параметров правильного тетраэдра в поток
     virtual void out(ofstream &ofst);
+
+    // Вычисление площади поверхности правильного тетраэдра
     virtual double sq_surf();
 };
 
-#endif //CSA_HW2_REGTET_H
+#endif //__regtet__
