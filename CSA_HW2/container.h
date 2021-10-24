e881ed4ffc16d49aa8caae3a9edf0b3e9324f696
Maksim Romanov <67387151+matttax@users.noreply.github.com>
2021-10-24 21:21:18 +0300

    Update container.h

--- CSA_HW2/container.h
+++ CSA_HW2/container.h
@@ -1,22 +1,30 @@
+// container.h содержит тип данных, представляющий контейнер
+
 #include "shape.h"
 
-#ifndef CSA_HW2_CONTAINER_H
-#define CSA_HW2_CONTAINER_H
+#ifndef __container__
+#define __container__
 
-
+// Контейнер на основе одномерного массива
 class container {
 private:
-    void clear();
     int len;
     shape *storage[10000];
 public:
     container();
     ~container();
 
+    // Ввод содержимого контейнера из указанного потока
     void in(ifstream &ifst);
+
+    // Случайный ввод содержимого контейнера
     void in_rand(int size);
+
+    // Вывод содержимого контейнера в указанный поток
     void out(ofstream &ofst);
+
+    // Сортировка контейнера прямым выбором
     void straightsel_sort();
 };
 
-#endif //CSA_HW2_CONTAINER_H
+#endif //__container__
