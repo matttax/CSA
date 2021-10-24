c2be06c512e26e150332edd8db982c95813ee0a8
Maksim Romanov <67387151+matttax@users.noreply.github.com>
2021-10-24 21:58:58 +0300

    Update shape.h

--- CSA_HW2/shape.h
+++ CSA_HW2/shape.h
@@ -1,19 +1,34 @@
+// shape.h - содержит описание обобщающей трёхмерной фигуры
+
 #include <fstream>
 
 using namespace  std;
 
-#ifndef CSA_HW2_SHAPE_H
-#define CSA_HW2_SHAPE_H
+#ifndef __shape__
+#define __shape__
 
+// Класс shape обобщает все имеющиеся фигуры
 class shape {
 public:
     virtual ~shape() {};
+
+    // Ввод обобщенной фигуры
     static shape *static_in(ifstream &ifst);
+
+    // Случайный ввод обобщенной фигуры
     static shape *static_in_rand();
+
+    // Виртуальный метод ввода обобщенной фигуры
     virtual void in(ifstream &ifst) = 0;
+
+    // Виртуальный метод ввода случайной фигуры
     virtual void in_rand() = 0;
+
+    // Виртуальный метод вывода обобщенной фигуры
     virtual void out(ofstream &ofst) = 0;
+
+    // Вычисление площади поверхности обобщенной фигуры
     virtual double sq_surf() = 0;
 };
 
-#endif //CSA_HW2_SHAPE_H
+#endif //__shape__
