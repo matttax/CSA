c376acf8b57ebb3c6e161a15c295a498524737ad
Maksim Romanov <67387151+matttax@users.noreply.github.com>
2021-10-24 21:32:02 +0300

    Update parallelepiped.cpp

--- CSA_HW2/parallelepiped.cpp
+++ CSA_HW2/parallelepiped.cpp
@@ -1,19 +1,25 @@
+// rectangle.cpp - содержит методы параллелепипеда
+
 #include "parallelepiped.h"
 
+// Ввод параметров параллелепипеда из файла
 void parallelepiped::in(ifstream &ifdt) {
     ifdt >> a >> b >> c;
 }
 
+// Случайная генерация параметров параллелепипеда
 void parallelepiped::in_rand() {
     a = random20();
     b = random20();
     c = random20();
 }
 
+// Вычисление площади поверхности параллелепипеда
 double parallelepiped::sq_surf() {
     return double(2 * (a * b + b * c + c * a));
 }
 
+// Вывод параметров параллелепипеда в поток
 void parallelepiped::out(ofstream &ofst) {
     ofst << "It is Parallelepiped: a = " << a
     << ", b = " << b
